bead431b3fcbff742bfc56d7cfdeb0f178f1bd5c
Dmytro Harasiuk <garasyuk2006@gmail.com>
2026-04-12 22:01:51 +0200

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"godaba/internal/db"
 	"godaba/internal/ui"
 
 	tea "charm.land/bubbletea/v2"
 )
 
 func main() {
-	m := ui.NewModel()
+	// conection := fmt.Sprintf("postgres://%s:%s@%s:%s?sslmode=prefer", "localhost", "5432", "postgres", "12345678")
+	connection := "postgres://postgres:12345678@localhost:5432?sslmode=prefer"
+	pg := db.NewExplorer("postgres", connection)
+	m := ui.NewModel(pg)
 	program := tea.NewProgram(m)
 	program.Run()
 }
